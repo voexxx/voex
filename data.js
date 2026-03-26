8cc4ace9978623d067afcbbd2d944ed1e3cd21a1
voexxx <s7uan@outlook.com>
2026-03-26 18:49:53 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -1,21 +1,124 @@
 const DATABASE = {
-  news: [
-  ],
+  news: [],
 
   teams: [
     {
       id: "Ventus Axi",
       rank: "1",
       name: "Ventus Axi",
-      meta: "FACEIT LVL ",
-      badge: "—",
+      meta: "FACEIT LVL",
+      badge: "0",
+      points: 0,
 
       players: ["m0nday", "squan", "v1ns", "SAQik444", "Yud0qq"],
+      achievements: [],
+      otherPlayers: [""]
+    },
 
+    {
+      id: "Burmalda",
+      rank: "2",
+      name: "Burmalda",
+      meta: "FACEIT LVL",
+      badge: "0",
+      points: 0,
+
+      players: ["V1zer", "Wackzzy", "hyse1n", "kashap", "LatypOFF"],
+      achievements: [],
+      otherPlayers: ["Kv1zzy"]
+    },
+
+    {
+      id: "WhiteTeam",
+      rank: "3",
+      name: "White team",
+      meta: "FACEIT LVL",
+      badge: "0",
+      points: 0,
+
+      players: ["ecstasy", "Tod0999", "MartyHie", "nikis", "Everest_Rus"],
+      achievements: [],
+      otherPlayers: ["bigkidswag", "depre??ed", "Luck1", "k", "Virtual_child1"]
+    },
+
+    {
+      id: "AsteriaTeam",
+      rank: "4",
+      name: "Asteria Team",
+      meta: "FACEIT LVL",
+      badge: "0",
+      points: 0,
+
+      players: ["Arshadow", "kogs1e", "ic3mice", "exe.Team", "rabotnik"],
+      achievements: [],
+      otherPlayers: ["dosia666", "Tre3zix", "samorezz", "sos1ska", "n3yzee","shyso","Magolez","Kl1mat","MoWee"]
+    },
+
+    {
+      id: "Team61",
+      rank: "5",
+      name: "Team 61",
+      meta: "FACEIT LVL",
+      badge: "0",
+      points: 0,
+
+      players: ["kyrlin", "Altreezz", "Kurok1sh1", "lov3zzy", "farrrr"],
+      achievements: [],
+      otherPlayers: ["VaRked666"]
+    },
+
+    {
+      id: "MVTeam",
+      rank: "6",
+      name: "MVTeam",
+      meta: "FACEIT LVL",
+      badge: "0",
+      points: 0,
+
+      players: ["m0nst0r", "Bat1r", "Alalkai", "RAVEN", "Topy"],
       achievements: [
+        "RIEM RIO - 1 place",
+        "Aventus Cup - 3-4 place",
+        "W Starladder - 3 place"
       ],
+      otherPlayers: ["Dqzz", "Volcane", "Koku", "Whitegodofd"]
+    },
 
-      otherPlayers: ["player6", "player7", "player8", "player9", "player10"]
+    {
+      id: "Lumen Novara",
+      rank: "7",
+      name: "Lumen Novara",
+      meta: "FACEIT LVL",
+      badge: "0",
+      points: 0,
+
+      players: ["sh3f", "wiggzes", "neetsky", "Dimonch4k", ""],
+      achievements: [],
+      otherPlayers: ["Romario","Shadow","X1do","Immortal","Vinrise"]
+    },
+    {
+      id: "Team Primus",
+      rank: "8",
+      name: "Team Primus",
+      meta: "FACEIT LVL",
+      badge: "0",
+      points: 0,
+
+      players: ["novaprospekt", "gsmod04", "ggg", "kuro", "swokinz"],
+      achievements: [],
+      otherPlayers: ["Hyp3rs","Hoopz"]
+    },
+    {
+      id: "BCW TEAM",
+      rank: "9",
+      name: "BCW TEAM",
+      meta: "FACEIT LVL",
+      badge: "0",
+      points: 0,
+
+      players: ["z3pp", "el1v1o", "flew", "vut1an", "rinex"],
+      achievements: [],
+      otherPlayers: []
     }
   ],
 
