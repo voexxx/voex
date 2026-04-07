46d1ab14f02682c44f329076d399a518727906e8
voexxx <s7uan@outlook.com>
2026-04-07 20:48:58 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -1,67 +1,56 @@
 const DATABASE = {
-  news: [],
+  news: [
+  {
+    id: 1,
+    cat: "NEWS",        // категория
+    date: "31 МАР",       // дата
+    title: "Скоро начало",
+    desc: "Хочу сказать что система уже через пару дней будет работать",
+    featured: true        // если хочешь выделить новость — ставь true
+  }
+],
 
   teams: [
     {
       id: "Ventus Axi",
-      rank: "1",
       name: "Ventus Axi",
       meta: "FACEIT LVL",
-      badge: "0",
-      points: 0,
-
-      players: ["m0nday", "squan", "v1ns", "SAQik444", "Yud0qq"],
+      players: ["m0nday", "JaTz", "v1ns", "argyle", ""],
       achievements: [],
-      otherPlayers: [""]
+      otherPlayers: ["Yud0qq"]
     },
 
     {
       id: "Burmalda",
-      rank: "2",
       name: "Burmalda",
       meta: "FACEIT LVL",
-      badge: "0",
-      points: 0,
-
-      players: ["V1zer", "Wackzzy", "hyse1n", "kashap", "LatypOFF"],
+      players: ["V1zer", "Wackzzy", "Soplenok", "dexperon", "LatypOFF"],
       achievements: [],
-      otherPlayers: ["Kv1zzy"]
+      otherPlayers: ["haku666"]
     },
 
     {
       id: "WhiteTeam",
-      rank: "3",
       name: "White team",
       meta: "FACEIT LVL",
-      badge: "0",
-      points: 0,
-
       players: ["ecstasy", "Tod0999", "MartyHie", "nikis", "Everest_Rus"],
       achievements: [],
       otherPlayers: ["bigkidswag", "depre??ed", "Luck1", "k", "Virtual_child1"]
     },
 
     {
-      id: "AsteriaTeam",
-      rank: "4",
-      name: "Asteria Team",
+      id: "Asteria Black",
+      name: "Asteria Black",
       meta: "FACEIT LVL",
-      badge: "0",
-      points: 0,
-
-      players: ["Arshadow", "kogs1e", "ic3mice", "exe.Team", "rabotnik"],
+      players: ["MoWee", "s0lkes", "aiwqq", "FR1ZYY", "Dosia"],
       achievements: [],
-      otherPlayers: ["dosia666", "Tre3zix", "samorezz", "sos1ska", "n3yzee","shyso","Magolez","Kl1mat","MoWee"]
+      otherPlayers: ["samorezz","Kl1mat"]
     },
 
     {
       id: "Team61",
-      rank: "5",
       name: "Team 61",
       meta: "FACEIT LVL",
-      badge: "0",
-      points: 0,
-
       players: ["kyrlin", "Altreezz", "Kurok1sh1", "lov3zzy", "farrrr"],
       achievements: [],
       otherPlayers: ["VaRked666"]
@@ -69,70 +58,124 @@ const DATABASE = {
 
     {
       id: "MVTeam",
-      rank: "6",
       name: "MVTeam",
       meta: "FACEIT LVL",
-      badge: "0",
-      points: 0,
-
-      players: ["m0nst0r", "Bat1r", "Alalkai", "RAVEN", "Topy"],
+      players: ["m0nst0r", "Dqzz", "Alalkai", "RAVEN", "Topy"],
       achievements: [
         "RIEM RIO - 1 place",
         "Aventus Cup - 3-4 place",
         "W Starladder - 3 place"
       ],
-      otherPlayers: ["Dqzz", "Volcane", "Koku", "Whitegodofd"]
+      otherPlayers: ["Volcane", "Koku", "S1eep"]
     },
-
     {
       id: "Lumen Novara",
-      rank: "7",
       name: "Lumen Novara",
       meta: "FACEIT LVL",
-      badge: "0",
-      points: 0,
-
-      players: ["sh3f", "wiggzes", "neetsky", "Dimonch4k", ""],
+      players: ["sh3f", "wiggzes", "neetsky", "Dimonch4k", "shiy"],
       achievements: [],
-      otherPlayers: ["Romario","Shadow","X1do","Immortal","Vinrise"]
+      otherPlayers: []
     },
     {
       id: "Team Primus",
-      rank: "8",
       name: "Team Primus",
       meta: "FACEIT LVL",
-      badge: "0",
-      points: 0,
-
       players: ["novaprospekt", "gsmod04", "ggg", "kuro", "swokinz"],
       achievements: [],
       otherPlayers: ["Hyp3rs","Hoopz"]
     },
     {
       id: "BCW TEAM",
-      rank: "9",
       name: "BCW TEAM",
       meta: "FACEIT LVL",
-      badge: "0",
-      points: 0,
-
       players: ["z3pp", "el1v1o", "flew", "vut1an", "rinex"],
       achievements: [],
       otherPlayers: []
+    },
+    {
+      id: "Team Silvers",
+      name: "Team Silvers",
+      meta: "FACEIT LVL",
+      players: ["ZyuZya","timaerror","Kasumi","Z3roX02","Bunar"],
+      achievements: ["2-е место Aventus Cup","8-4 место IPE Major"],
+      otherPlayers: ["VkidGames","ByShine","rix"]
+    },
+    {
+      id: "Team Silvers Academy",
+      name: "Team Silvers Academy",
+      meta: "FACEIT LVL",
+      players: ["n1ght","fantabym","guzzy","gili3cs","Ehone1j"],
+      achievements: [],
+      otherPlayers: ["b1ndo"]
+    },
+    {
+      id: "Blood Owners",
+      name: "Blood Owners",
+      meta: "FACEIT LVL",
+      players: ["Cry","komuqi","mAVR1K","Semen41K","kaiori"],
+      achievements: ["Reload Cup 3 - place"],
+      otherPlayers: ["mesure","Lwftaze","TheKervich","pilotf14","Toys"]
+    },
+    {
+      id: "Exot Team",
+      name: "Exot Team",
+      meta: "FACEIT LVL",
+      players: ["VARNEX","VSnipeX","Skyooo","Pr0f1d","Yuki"],
+      achievements: [],
+      otherPlayers: ["magixX_2","Mramor"]
+    },
+    {
+      id: "Team Expoo",
+      name: "Team Expoo",
+      meta: "FACEIT LVL",
+      players: ["shintrix","zelofa1n","wedding","sw1k","NEXT_TIME"],
+      achievements: ["3-4 place BLASTY OPEN WINTER","2nd place BLASTY PRO LEAGUE 2","3-4 place TABURETKA CUP","3-4 place RAZE CUP SEASON 5"],
+      otherPlayers: ["MDB","Lord_Elite","Xandow"]
+    },
+    {
+      id: "Lumen Novara Academy",
+      name: "Lumen Novara Academy",
+      meta: "FACEIT LVL",
+      players: ["Romario","Shadow","X1do","Immortal","Vinrise"],
+      achievements: [],
+      otherPlayers: ["Sday","King666","Hatyykk"]
+    },
+    {
+      id: "Barebuh Team",
+      name: "Barebuh Team",
+      meta: "FACEIT LVL",
+      players: ["aori","r3kn","Goidmen","blessedrecode","Leha epta"],
+      achievements: [],
+      otherPlayers: ["Popo4ka"]
+    },
+    {
+      id: "Warm Ray Team",
+      name: "Warm Ray Team",
+      meta: "FACEIT LVL",
+      players: ["Blex","k1llobyte","4shist","s1baa","methody"],
+      achievements: [],
+      otherPlayers: [""]
+    }, 
+    {
+      id: "HYDRA eSports",
+      name: "HYDRA eSports",
+      meta: "FACEIT LVL",
+      players: ["st0nks","1nference","z1pqt","Lapatasion","Rawlod"],
+      achievements: [],
+      otherPlayers: ["theYELLSS"]
     }
   ],
 
   matches: [
     {
       id: 1,
-      team1: "Ventus Axi",
-      team2: "Unknown Team",
+      team1: "Burmalda",
+      team2: "Lumen Novara",
       score: "2:0",
       date: "25 МАР",
       format: "BO3",
-      maps: ["Mirage 13:7", "Dust2 13:10"]
+      maps: ["Mirage 13:6", "Nuke 13:7"]
     }
   ]
 };
-
 
